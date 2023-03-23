3d725ce521e2d9f13e41afb5236fe30d9d70dffe
kflinnn <kflinncoding@gmail.com>
2023-03-22 20:47:09 -0400

    updated category-routes and some product-routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,9 +4,17 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+router.get('/', async (req, res) => {
+  // find all products and include its associated Category and Tag data
+  try { 
+    // find all categories and includes its associated Products
+   const productData = await Product.findAll({ 
+    include: [{ Category }, { Tag }]
+  });
+   res.status(200).json(productData);
+  } catch (err) {
+   res.status(500).json(err);
+  }
 });
 
 // get one product
