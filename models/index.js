bb865b16394663f8902ef93ea87c6d075edb5b8c
kflinnn <kflinncoding@gmail.com>
2023-04-01 20:06:34 -0400

    debugged errors from running seed

--- models/index.js
+++ models/index.js
@@ -20,22 +20,18 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 
 Product.belongsToMany(Tag, {
-  through: {
-    model: ProductTag,
-    unique: false
-  },
-  as: 'product_tag'
+  through: ProductTag,
+  as: 'product_tags',
+  foreignKey: 'product_id',
 });
 
 // Tags belongToMany Products (through ProductTag)
 
 Tag.belongsToMany(Product, {
-  through: {
-    model: ProductTag,
-    unique: false
-  },
-  as: 'tag_product'
-});
+  through: ProductTag,
+  as: 'product_tags',
+  foreignKey: 'tag_id',
+})
 
 module.exports = {
   Product,
